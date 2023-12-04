9c613c9ec8eb4e7829d3fd8ca456549951b217f6
Bogdan Vlad <bogdan@genez.io>
2023-12-04 12:34:40 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as test from "test-getting-started-genezio-typescript-newest_us-east-1";
+import { TestNewType } from "test-getting-started-genezio-typescript-newest_us-east-1/cjs/taskService.sdk";
 
 function App() {
+  const x: TestNewType | undefined = undefined;
+  test.TaskService.createTask("token", "title");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reloa d.
+          {}
         </p>
         <a
           className="App-link"
